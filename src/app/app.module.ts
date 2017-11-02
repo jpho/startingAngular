53637634f08a3097fb9d4a67f36af2415174dea1
joseph yang <josephpho@gmail.com>
2017-11-02 10:16:25 -0500

    middle of 2g video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent }  from './app.component';
+import { ProductListComponent }  from './products/product-list.component'; // to make it available
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+     BrowserModule, 
+     FormsModule
+    ],
+  declarations: [ 
+    AppComponent,
+    ProductListComponent // tie it all in 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
